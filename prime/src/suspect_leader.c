10c15da4e9a5005af6a4f3ce8830d94401c7adc3
Amy Babay <amy.e.babay@gmail.com>
2025-01-09 17:08:22 -0500

    bugfix in suspect leader - line of code setting up array to sort reported_tat values got deleted in 2.1 release (!); needed to re-add Found by Aren's test case 4

--- prime/src/suspect_leader.c
+++ prime/src/suspect_leader.c
@@ -239,6 +239,7 @@ void SUSPECT_TAT_Measure_Periodically(int dummy, void *dummyp)
 
 void SUSPECT_Process_TAT_Measure(signed_message *mess)
 {
+    int i;
     double tats[VAR.Num_Servers+1];
     double prev_leader, accept;
     tat_measure_message *measure;
@@ -252,6 +253,10 @@ void SUSPECT_Process_TAT_Measure(signed_message *mess)
 
     if (measure->max_tat > DATA.SUSP.reported_tats[mess->machine_id]) {
         DATA.SUSP.reported_tats[mess->machine_id] = measure->max_tat;
+    }
+
+    for (i = 1; i <= VAR.Num_Servers; i++) {
+        tats[i] = DATA.SUSP.reported_tats[i];
     }
     
     qsort((void*)(tats+1), VAR.Num_Servers, sizeof(double), doublecmp);
